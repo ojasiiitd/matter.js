cc2868e232e4faba5cb45053ba21f334f61340e6
ojasiiitd <ojassaxena03@gmail.com>
2019-07-02 16:11:23 +0530

    Click to add balls

--- JS/script.js
+++ JS/script.js
@@ -18,23 +18,25 @@ var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 var leftWall = Bodies.rectangle(0, 0, 30, 1200 , { isStatic: true });
 var rightWall = Bodies.rectangle(canvas.width, 0, 30, 1200, { isStatic: true });
 var balls = [];
+var options = {
+    frictionAir: 0.01 , 
+    friction: 0.2 , 
+    restitution: 0.8 ,
+    force: {
+        x: .4,
+        y: .4
+    }
+};
 
-var newBall = Bodies.circle(100 , 100 , 40);
+canvas.addEventListener("click" , function() {
+var newBall = Bodies.circle(event.clientX ,event.clientY , 40 , options);
+console.log(newBall);
 balls.push(newBall);
-var newBall = Bodies.circle(160 , 100 , 40);
-balls.push(newBall);
-var newBall = Bodies.circle(220 , 100 , 40);
-balls.push(newBall);
-var newBall = Bodies.circle(280 , 100 , 40);
-balls.push(newBall);
-var newBall = Bodies.circle(340 , 100 , 40);
-balls.push(newBall);
-var newBall = Bodies.circle(400 , 100 , 40);
-balls.push(newBall);
+World.add(engine.world, balls[balls.length-1]);
+});
 
 // add all of the bodies to the world
 World.add(engine.world, [ground , leftWall , rightWall]);
-World.add(engine.world, balls);
 
 // run the engine
 Engine.run(engine);
