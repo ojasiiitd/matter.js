41c70c56979e59f6f718d5c39da374a268c03e5d
ojasiiitd <ojassaxena03@gmail.com>
2019-07-02 19:33:52 +0530

    Adding click-polygons with a force

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,59 @@
-// module aliases
+var win = window,
+    doc = document,
+    htm = doc.documentElement,
+    body = doc.getElementsByTagName("body")[0];
+    windowWidth = win.innerWidth || htm.clientWidth || body.clientWidth,
+    windowHeight = win.innerHeight || htm.clientHeight || body.clientHeight;
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
     Bodies = Matter.Bodies;
-
-// create an engine
 var engine = Engine.create();
-
-// create a renderer
 var render = Render.create({
     element: document.body,
     engine: engine
 });
 
 var canvas = document.getElementsByTagName("canvas")[0];
-var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
-var leftWall = Bodies.rectangle(0, 0, 30, 1200 , { isStatic: true });
-var rightWall = Bodies.rectangle(canvas.width, 0, 30, 1200, { isStatic: true });
-var balls = [];
-var options = {
-    frictionAir: 0.01 , 
-    friction: 0.2 , 
-    restitution: 0.8 ,
-    force: {
-        x: .4,
-        y: .4
-    }
-};
+canvas.width = windowWidth;
+canvas.height = windowHeight - 7;
 
-canvas.addEventListener("click" , function() {
-var newBall = Bodies.circle(event.clientX ,event.clientY , 40 , options);
-console.log(newBall);
-balls.push(newBall);
-World.add(engine.world, balls[balls.length-1]);
-});
-
-// add all of the bodies to the world
+var ground = Bodies.rectangle(canvas.width/2 , canvas.height-(60/2) , canvas.width , 60 , { isStatic: true }),
+    leftWall = Bodies.rectangle(15 , canvas.height/2 , 30 , canvas.height , { isStatic: true }) ,
+    rightWall = Bodies.rectangle(canvas.width-30 , canvas.height/2 , 30 , canvas.height , { isStatic: true });
 World.add(engine.world, [ground , leftWall , rightWall]);
 
-// run the engine
-Engine.run(engine);
+var elements = [];
+canvas.addEventListener("click" , createElements);
+function createElements()
+{
+    var x = event.clientX , y = event.clientY ,
+        sides = getRandomInt(3 , 8);
+        radius = getRandomInt(30 , 80);
+        opts = {
+            friction: 0,
+            restitution: 1,
+            force: {
+                x: getRandomFloat(-.7 , .7)
+            },
+        };
+        add = Bodies.polygon(x , y , sides , radius , opts);
+    console.log(add);
+    World.add(engine.world , add);
+    elements.push(add);
+}
 
-// run the renderer
+function getRandomInt(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function getRandomFloat(min, max)
+{
+    return (Math.random() * (max - min + 1) + min);
+}
+
+Engine.run(engine);
 Render.run(render);
