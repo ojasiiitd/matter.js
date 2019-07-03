6f12212266c1bd18d5a3605682aa33f7f38e95f1
ojasiiitd <ojassaxena03@gmail.com>
2019-07-03 13:25:06 +0530

    Adding constraints

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,8 @@ var win = window,
 var Engine = Matter.Engine,
     Render = Matter.Render,
     World = Matter.World,
-    Bodies = Matter.Bodies;
+    Bodies = Matter.Bodies,
+    Constraint = Matter.Constraint;
 var engine = Engine.create();
 var render = Render.create({
     element: document.body,
@@ -21,70 +22,97 @@ var render = Render.create({
 var canvas = document.getElementsByTagName("canvas")[0];
 canvas.width = windowWidth;
 canvas.height = windowHeight;
+var ctx = canvas.getContext("2d");
 
 var wallopts = {
     render: {
-        fillStyle: "green",
+        fillStyle: "rgb(43, 43, 43)",
     },
     friction: 0,
     isStatic: true
 };
 
-var ground = Bodies.rectangle(canvas.width/2 , canvas.height+120 , canvas.width , 300 , wallopts),
+var ground = Bodies.rectangle(canvas.width/2 , canvas.height+120 , canvas.width , 300 , wallopts);
     leftWall = Bodies.rectangle(-120 , canvas.height/2 , 300 , canvas.height , wallopts) ,
     rightWall = Bodies.rectangle(canvas.width+110 , canvas.height/2 , 300 , canvas.height , wallopts);
-World.add(engine.world, [ground , leftWall , rightWall]);
+// World.add(engine.world, [ground , leftWall , rightWall]);
 
-var slide = [];
-slide.push(Bodies.rectangle(300 , 100 , 1500 , 10 , {
+var opts = {
     render: {
-        fillStyle: "green",
+        fillStyle: "yellow",
     },
-    isStatic: true,
-    angle: 16*Math.PI/180
-    }));
-slide.push(Bodies.rectangle(1460 , 350 , 900 , 10 , {
-    render: {
-        fillStyle: "green",
-    },
-    isStatic: true,
-    angle: -16*Math.PI/180
-    }));
-slide.push(Bodies.rectangle(300 , 500 , 1500 , 10 , {
-    render: {
-        fillStyle: "green",
-    },
-    isStatic: true,
-    angle: 16*Math.PI/180
-    }));
-World.add(engine.world , slide);
+    friction: 0,
+    restitution: 1,
+};
 
-var elements = [];
-canvas.addEventListener("mousemove" , createElements);
-function createElements()
-{
-    console.log(event);
-    var x = event.clientX , y = event.clientY ,
-        sides = getRandomInt(3 , 8);
-        radius = getRandomInt(1 , 10);
-        opts = {
-            render: {
-                fillStyle: "slateblue",
-            },
+var box = Bodies.rectangle(500 , 300 , 60 , 40 , {
+    render: {
+        fillStyle: "yellow",
+    },
+    friction: 0,
+    restitution: 1,
+    isStatic : true
+}),
+    ball = Bodies.circle(820 , 80 , 40 , opts);
 
-            friction: 0,
-            restitution: 1,
-            mass: 10,
-            force: {
-                x: getRandomFloat(-.7 , .7),
-                y: getRandomFloat(-.7 , .7)
-            },
-        };
-        add = Bodies.circle(x , y , radius , opts);
-    // console.log(add);
-    World.add(engine.world , add);
-    elements.push(add);
-}
+World.add(engine.world , [box , ball]);
+
+var constraintOptions = {
+    bodyA: box,
+    bodyB: ball,
+    length: 200,
+    stiffness: .01,
+};
+
+var spring = Constraint.create(constraintOptions);
+World.add(engine.world , spring);
+
+// var slide = [];
+// slide.push(Bodies.rectangle(300 , 100 , 1500 , 10 , {
+//     render: {
+//         fillStyle: "green",
+//     },
+//     isStatic: true,
+//     angle: 16*Math.PI/180
+//     }));
+// slide.push(Bodies.rectangle(1460 , 350 , 900 , 10 , {
+//     render: {
+//         fillStyle: "red",
+//     },
+//     isStatic: true,
+//     angle: -16*Math.PI/180
+//     }));
+// slide.push(Bodies.rectangle(300 , 500 , 1500 , 10 , {
+//     render: {
+//         fillStyle: "white",
+//     },
+//     isStatic: true,
+//     angle: 16*Math.PI/180
+//     }));
+// World.add(engine.world , slide);
+
+// canvas.addEventListener("mousemove" , createElements);
+// function createElements()
+// {
+//     var x = event.clientX , y = event.clientY ,
+//         sides = getRandomInt(3 , 8);
+//         radius = getRandomInt(7 , 17);
+//         opts = {
+//             render: {
+//                 fillStyle: "yellow",
+//             },
+
+//             friction: 0,
+//             restitution: 1,
+//             mass: 15,
+//             force: {
+//                 x: getRandomFloat(-1 , 1),
+//                 y: getRandomFloat(-1 , 1)
+//             },
+//         };
+//         add = Bodies.polygon(x , y , sides , radius , opts);
+//     World.add(engine.world , add);
+// }
 
 function getRandomInt(min, max)
 {
